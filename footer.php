403ad8d8ff761a3590ebf3d316e1443f9e923bc4
David Sword <davidsword@Betsy.local>
2018-04-15 20:39:49 -0700

    📦 changed host credits for soon to be change

--- footer.php
+++ footer.php
@@ -2,12 +2,12 @@
 	<footer>
 		<p>
 			<a href='mailto:david@davidsword.ca'>david@davidsword.ca</a> &nbsp; | &nbsp;
-			Built in 🇨🇦 with 
+			Built in 🇨🇦 with
 			<a href='http://a.co/aiKUMd3' target='_Blank'>coffee</a>,
 			<a href='https://wordpress.org' target='_Blank'>Wordpress</a>,
-			<a href='https://atom.io' target='_Blank'>Atom</a>,
 			<a href='https://github.com/WordPress/gutenberg' target='_Blank'>Gutenberg</a>,
-			<a href='https://aws.amazon.com/lightsail/' target='_Blank'>Lightsail</a>,
+			<a href='https://getflywheel.com/' target='_Blank'>Flywheel</a>,
+			<a href='https://atom.io' target='_Blank'>Atom</a>,
 			<a href='https://davidsword.ca/uses/' target='_Blank'>&amp; more</a>
 		</p>
 	</footer>

2bdcfbefdd4eb6644449aeebc2803dbe68b699fd
David Sword <david@davidsword.ca>
2018-05-11 21:39:14 -0700

    minor js change

--- assests/js/main.js
+++ assests/js/main.js
@@ -40,9 +40,10 @@ jQuery(document).ready(function() { // executes when HTML-Document is loaded and
 		jQuery('.menu-item-object-projects').addClass('current-menu-item'); // "PROJECTS"
 	}
 	if (jQuery('body').hasClass('post-type-archive-images') ||
-		jQuery('body').hasClass('post-type-archive-art') ||
+		jQuery('body').hasClass('post-type-archive-art')
 		//jQuery('body').hasClass('post-type-archive-ramblings') ||
-		jQuery('body').hasClass('single-ramblings')) {
+		//jQuery('body').hasClass('single-ramblings'))
+		{
 			jQuery('.menu-item-4707').addClass('current-menu-item'); // "ABOUT"
 	}
 	if (jQuery('body').hasClass('single-post') ||

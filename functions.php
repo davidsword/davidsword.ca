4e9f87ca4cf16069c05087cf0900cd0558a875d4
David Sword <david@davidsword.ca>
2018-06-19 15:23:24 -0700

    chanbged: version for cache bust

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ include('inc/ds-helpers.php');
 /* ----------------------------------------------------------------------------------------------------- */
 
 $swrdbs = [
-	'theme_varient' => '2.0.8',
+	'theme_varient' => '2.0.9',
 	'dev' => false,
 	'dev_user_id' => 1,
 	'hero_title' => true,

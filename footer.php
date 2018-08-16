f94605bc0046100a9670b5ca39814e2af0b3520f
David Sword <david@davidsword.ca>
2018-08-15 20:48:52 -0700

    changed: footer copy

--- footer.php
+++ footer.php
@@ -1,14 +1,18 @@
 <?php defined('ABSPATH') || exit; global $swrdbs; ?>
 	<footer>
 		<p>
-			<a href='mailto:david@davidsword.ca'>david@davidsword.ca</a> &nbsp; | &nbsp;
+			<a href='mailto:david@davidsword.ca'>david@davidsword.ca</a>
+
+			&nbsp; | &nbsp;
+
+			Proudly powered by
+			<a href='http://a.co/6rrglTj' target='_Blank'>coffee</a> &amp;
+			<a href='https://wordpress.org' target='_Blank'>Wordpress</a>
+
+			&nbsp; | &nbsp;
+
 			Built in 🇨🇦 with
-			<a href='http://a.co/aiKUMd3' target='_Blank'>coffee</a>,
-			<a href='https://wordpress.org' target='_Blank'>Wordpress</a>,
-			<a href='https://github.com/WordPress/gutenberg' target='_Blank'>Gutenberg</a>,
-			<a href='https://aws.amazon.com/lightsail/' target='_Blank'>AWS Lightsail</a>,
-			<a href='https://atom.io' target='_Blank'>Atom</a>,
-			<a href='https://davidsword.ca/uses/' target='_Blank'>&amp; more</a>
+			<a href='https://davidsword.ca/uses/' target='_Blank'>all this stuff</a>
 		</p>
 	</footer>
 

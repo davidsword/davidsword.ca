2d2a958c95b21a192b862e318d592397a6844e9b
David Sword <david@davidsword.ca>
2019-03-16 22:03:45 -0700

    fix single post post_titles not hyperlinked
    no reason for it, looks bad too.
    
    You have arrived at your destination.
    Why leave to go to your destination.

--- partials/content-standard.php
+++ partials/content-standard.php
@@ -1,4 +1,14 @@
-<h2><a href='<?php echo get_permalink(); ?>'><?php the_title(); ?> &raquo;</a></h2>
+<h2>
+    <?php
+    if ( ! is_single() ) {
+        ?>
+        <a href='<?php echo get_permalink(); ?>'><?php the_title(); ?> &raquo;</a>
+    <?php
+    } else {
+        the_title();
+    }
+    ?>
+</h2>
 <div class='content'>
     <?php
         // Featured image.

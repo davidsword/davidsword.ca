27dee87c1a085e8f7ca1ff2d947a232eb14e217a
David Sword <david@davidsword.ca>
2019-03-16 23:58:00 -0700

    change improve comment style

--- comments.php
+++ comments.php
@@ -22,26 +22,7 @@ if ( post_password_required() ) {
 
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
-			<?php
-			$comments_number = get_comments_number();
-			if ( '1' === $comments_number ) {
-				/* translators: %s: post title */
-				printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentyfifteen' ), get_the_title() );
-			} else {
-				printf(
-					/* translators: 1: number of comments, 2: post title */
-					_nx(
-						'%1$s thought on &ldquo;%2$s&rdquo;',
-						'%1$s thoughts on &ldquo;%2$s&rdquo;',
-						$comments_number,
-						'comments title',
-						'twentyfifteen'
-					),
-					number_format_i18n( $comments_number ),
-					get_the_title()
-				);
-			}
-			?>
+			Comments
 		</h2>
 
 		<ol class="comment-list">
@@ -49,7 +30,8 @@ if ( post_password_required() ) {
 			wp_list_comments(array(
 				'style'       => 'ol',
 				'short_ping'  => true,
-				'avatar_size' => 56,
+				'avatar_size' => 60,
+				'format'      => 'html5'
 			));
 			?>
 		</ol><!-- .comment-list -->

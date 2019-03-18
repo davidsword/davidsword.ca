2b7e4aeb17c6d1e13a72262c32b21785146d3dfe
David Sword <david@davidsword.ca>
2019-03-18 08:43:43 -0700

    change 'not found' result messages

--- partials/article-not-found.php
+++ partials/article-not-found.php
@@ -1,5 +1,15 @@
+<?php
+/**
+ * Article template for posts not found.
+ *
+ * Commonly used in Search or an empty category/term.
+ *
+ * @package davidsword-ca
+ */
+?>
 <article <?php post_class(); ?>>
 	<div class='entry'>
-		<h2>🤷🏼‍♂️ No posts found</h2>
+		<h2>Uh oh.</h2>
+		<p>It seems like the post(s) you tried to reach have either been removed, renamed, or didn’t exist in the first place. You can return to the home page of the website by clicking <a href='<?php home_url(); ?>'>here</a>.</p>
 	</div>
 </article>

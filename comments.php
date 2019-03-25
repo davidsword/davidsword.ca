387e6e93b504b64b4e947c14e30efbb9f485994b
David Sword <david@davidsword.ca>
2019-03-24 17:36:31 -0700

    change clean up comments template

--- comments.php
+++ comments.php
@@ -5,18 +5,16 @@
  * The area of the page that contains both current comments
  * and the comment form.
  *
- * @TODO this was ripped from Twenty-x theme with little care, needs attention.
- *
  * @package davidsword-ca
  */
 
-// Go away, VIP only.
-if ( post_password_required() ) {
+// maybe bail..
+if ( ! comments_open() || post_password_required() ) {
 	return;
 }
 ?>
 
-<div id="comments" class="comments-area">
+<div id="comments">
 
 	<hr />
 
@@ -27,25 +25,18 @@ if ( post_password_required() ) {
 
 		<ol class="comment-list">
 			<?php
-			wp_list_comments(array(
+			wp_list_comments([
 				'style'       => 'ol',
 				'short_ping'  => true,
 				'avatar_size' => 60,
 				'format'      => 'html5',
 				'reply_text'  => 'Reply to this.'
-			));
+			]);
 			?>
 		</ol><!-- .comment-list -->
 
 	<?php endif; ?>
 
-	<?php
-	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-		?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentyfifteen' ); ?></p>
-	<?php endif; ?>
-
 	<?php comment_form(); ?>
 
-</div><!-- .comments-area -->
+</div><!-- #comments -->

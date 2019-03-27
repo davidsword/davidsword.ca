74d8a072f510c73cba567cec26dd62ff8747c8d8
David Sword <david@davidsword.ca>
2019-03-27 14:42:17 -0700

    📦 NEW: remove WP emojicon, rely on user instead

--- pluggable/dsca-pref.php
+++ pluggable/dsca-pref.php
@@ -67,3 +67,20 @@ add_action( 'manage_posts_custom_column', function( $column_name, $id ) {
 		echo '</a>';
 	}
 }, 999, 2);
+
+/**
+ * Remove WordPress emojicon, rely on users systems instead.
+ */
+add_action( 'init', function() {
+
+	remove_action( 'admin_print_styles',  'print_emoji_styles' );
+	remove_action( 'wp_head',             'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles',     'print_emoji_styles' );
+	remove_filter( 'wp_mail',             'wp_staticize_emoji_for_email' );
+	remove_filter( 'the_content_feed',    'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss',    'wp_staticize_emoji' );
+
+	add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+	add_filter( 'emoji_svg_url',    '__return_false' );
+} );

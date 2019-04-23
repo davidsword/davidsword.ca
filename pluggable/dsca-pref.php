4eb322e0ac8cd117e4d2d18bbbe41275542bd5c2
David Sword <david@davidsword.ca>
2019-04-23 16:45:34 -0700

    👌 IMPROVE: revert removal of emojicons

--- pluggable/dsca-pref.php
+++ pluggable/dsca-pref.php
@@ -75,6 +75,8 @@ add_action( 'manage_posts_custom_column', function( $column_name, $id ) {
  */
 add_action( 'init', function() {
 
+	return; // disable this, yes use emojicon.
+
 	remove_action( 'admin_print_styles',  'print_emoji_styles' );
 	remove_action( 'wp_head',             'print_emoji_detection_script', 7 );
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );

218700e17ec7157a8520155de11f8f81b9dd3e95
David Sword (Work) <davidsword@Betsy2.local>
2019-08-31 16:05:20 -0700

    fix broken path on main requires

--- functions.php
+++ functions.php
@@ -8,12 +8,12 @@
 defined( 'ABSPATH' ) || exit;
 
 // Load custom plugins.
-require get_stylesheet_directory() . 'pluggable/projects.php';
-require get_stylesheet_directory() . 'pluggable/dsca-pref.php';
-require get_stylesheet_directory() . 'pluggable/security.php';
-require get_stylesheet_directory() . 'pluggable/shortcodes.php';
-require get_stylesheet_directory() . 'pluggable/gists/gists.php';
-require get_stylesheet_directory() . 'pluggable/pseduo-post-format.php';
+require get_stylesheet_directory() . '/pluggable/projects.php';
+require get_stylesheet_directory() . '/pluggable/dsca-pref.php';
+require get_stylesheet_directory() . '/pluggable/security.php';
+require get_stylesheet_directory() . '/pluggable/shortcodes.php';
+require get_stylesheet_directory() . '/pluggable/gists/gists.php';
+require get_stylesheet_directory() . '/pluggable/pseduo-post-format.php';
 
 /**
  * Add various features for theme

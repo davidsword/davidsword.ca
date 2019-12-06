ae9d645ae440e28d16392a6e1abd76c037a3a0f7
David Sword <david.sword@automattic.com>
2019-12-06 07:34:59 -0800

    remove projects as cpt
    use them a post's instead. add the flag to posts.

--- pluggable/projects.php
+++ pluggable/projects.php
@@ -1,57 +1,9 @@
 <?php
 /**
- * Custom page for PROJECTs post type.
+ * Add on to PROJECT category.
  *
  * @package davidsword-ca
  */
-
-/**
- * Register post type
- */
-add_action( 'init' , function () {
-	$cpt_name = 'Project';
-	$cpt_slug = 'project';
-	$args = [
-		'labels'                => dsca_make_labels( $cpt_name ),
-		'public'                => true,
-		'publicly_queryable'    => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'query_var'             => true,
-		'rewrite'               => true,
-		'has_archive'           => 'projects',
-		'capability_type'       => 'post',
-		'hierarchical'          => false,
-		'menu_position'         => 5,
-		'menu_icon'             => 'dashicons-book',
-		'supports'              => [
-			'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'
-		],
-		'show_in_rest'          => true,
-		'rest_base'             => 'projects',
-		'rest_controller_class' => 'WP_REST_Posts_Controller',
-	];
-	register_post_type( $cpt_slug, $args );
-});
-
-/**
- * Set limit on front end main archive page.
- *
- * Projects are a big deal, and older projects aren't that great.
- * So reduce to just show 6 initally.
- */
-add_filter('pre_get_posts', function ( $query ) {
-
-	$is_project = isset( $query->query['post_type'] ) && 'project' === $query->query['post_type'];
-	$is_archive = ( 1 === $query->is_archive );
-
-	// ONLY ON FRONT END ARCHIVES PAGE.
-	if ( ! is_admin() && $is_project && $is_archive && ! isset( $query->query['posts_per_page'] ) ) {
-			$query->set( 'posts_per_page', '-1' );
-	}
-
-	return $query;
-});
 
 /**
  * Add custom tags for Projects
@@ -61,7 +13,7 @@ add_filter('pre_get_posts', function ( $query ) {
 add_action( 'init', function () {
 	$tax_name = 'Flag';
 	$tax_slug = 'flag';
-	register_taxonomy( $tax_slug, [ 'project' ], [
+	register_taxonomy( $tax_slug, [ 'post' ], [
 		'hierarchical' => false,
 		'labels' => [
 			'name' => $tax_name, 'taxonomy general name',
@@ -88,7 +40,7 @@ add_action( 'init', function () {
  * Add the Flag in the admin
  */
 add_filter( 'manage_projects_posts_columns', function( $columns ) {
-	$columns['flag'] = 'Flag';
+	$columns['flag'] = 'Project Flag';
 	return $columns;
 } );
 
@@ -124,56 +76,4 @@ function dsca_get_flag( $post_id = false, $only_slug = false ) {
 		return $only_slug ? $flag->slug : "<span class='gray'>#{$flag->slug}</span>";
 	}
 	return false;
-}
-
-/**
- * Add projects to main RSS feed.
- *
- * This may cause a bug in the future if anything else edits this.
- *
- * @param $qv array
- * @return array
- */
-add_filter('request', function ( $qv ) {
-	if ( isset( $qv['feed'] ) ) {
-		if ( ! isset( $qv['post_type'] ) ) {
-			$qv['post_type'] = [ 'post', 'project' ];
-		}
-	}
-	return $qv;
-} );
-
-/**
- * Allow featured image.
- */
-add_action( 'after_setup_theme', function () {
-	add_theme_support( 'post-thumbnails', [ 'project' ] );
-});
-
-/**
- * Dynamically Generate Labels for cpts admin interface.
- *
- * I'm really not sure why core doesn't do this.
- *
- * @TODO abstracting this is no longer needed w/ just one post type.
- *
- * @param string $cpt name of the CPT, singular.
- * @return array of labels
- */
-function dsca_make_labels( $cpt ) {
-	return [
-		'name'               => $cpt,
-		'singular_name'      => $cpt,
-		'add_new'            => 'Add New',
-		'add_new_item'       => 'Add New ' . $cpt,
-		'edit_item'          => 'Edit ' . $cpt,
-		'new_item'           => 'New ' . $cpt,
-		'all_items'          => 'All ' . $cpt,
-		'view_item'          => 'View ' . $cpt,
-		'search_items'       => 'Search ' . $cpt,
-		'not_found'          => 'No ' . $cpt . ' found',
-		'not_found_in_trash' => 'No ' . $cpt . ' found in Trash',
-		'parent_item_colon'  => '',
-		'menu_name'          => $cpt,
-	];
 }

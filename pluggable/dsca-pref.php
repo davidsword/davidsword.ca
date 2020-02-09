7d5e36be2a6c2dc7107c55f3f12f1ccac0ab1beb
David Sword <david.sword@automattic.com>
2020-02-09 12:21:28 -0800

    add thumbnails for all CPTs

--- pluggable/dsca-pref.php
+++ pluggable/dsca-pref.php
@@ -38,25 +38,32 @@ add_action( 'init' ,function () {
  * @param array  $columns from wp api.
  * @return array $columns for wp api.
  */
-function ds_makethumbnailcol( $columns ) {
+function ds_make_thumbnail_cols( $columns ) {
 	unset( $columns['date'] );
 	unset( $columns['comments'] );
 	unset( $columns['author'] );
-	$columns['img_thumbnail'] = '';
+	$columns['img_thumbnail'] = 'Ftr Img';
 	return $columns;
 }
-add_filter( 'manage_post_posts_columns', 'ds_makethumbnailcol' );
+
+add_action('init', function(){
+	$cpts = ['post','project','photo','status','page'];
+	foreach ($cpts as $cpt) {
+		add_filter( "manage_{$cpt}_posts_columns", 'ds_make_thumbnail_cols' );
+	}
+	add_action( 'manage_posts_custom_column', 'ds_make_thumbnail_cells', 999, 2);
+},11);
 
 /**
  * Display the post thumbnail in the edit page table for eaiser management
  */
-add_action( 'manage_posts_custom_column', function( $column_name, $id ) {
+function ds_make_thumbnail_cells( $column_name, $id ) {
 	if ( 'img_thumbnail' === $column_name ) {
 		echo "<a href='" . esc_url( get_edit_post_link() ) . "'>";
-		echo wp_kses_post( the_post_thumbnail( 'thumbnail', [ 'style' => 'max-width: 40px;height:auto' ] ) );
+		echo wp_kses_post( the_post_thumbnail( 'thumbnail', [ 'style' => 'max-width: 60px;height:auto' ] ) );
 		echo '</a>';
 	}
-}, 999, 2);
+}
 
 /**
  * Remove WordPress emojicon, rely on users systems instead.

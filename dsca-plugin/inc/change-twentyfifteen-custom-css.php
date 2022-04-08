1e7424c7c554a17ec56250103d6cd135c3008138
David Sword <1996370+davidsword@users.noreply.github.com>
2022-04-08 12:07:51 -0700

    another wave of changes to 2015 theme

--- dsca-plugin/inc/change-twentyfifteen-custom-css.php
+++ dsca-plugin/inc/change-twentyfifteen-custom-css.php
@@ -14,3 +14,31 @@ add_action( 'wp_enqueue_scripts', function() {
 	);
 
 }, 999999999 );
+
+
+
+add_action('wp_head', function() {
+
+	if ( 'Twenty Fifteen' !== wp_get_theme()->get('Name') )
+		return;
+
+	?>
+	<style>
+.social-navigation a[href*="stackexchange.com"]:before {
+	content: "";
+	background: url(<?= dsca_get_plugin_uri() ?>/assets/icon-stack-exchange.png) 30px 30px;
+	background-position: center;
+	background-size: contain;
+	width: 28px;
+	height: 28px;
+	display: inline-block;
+	transform: translateX(-1px) translateY(-3px);
+	opacity: 0.8;
+}
+
+.social-navigation li:hover a[href*="stackexchange.com"]:before {
+	opacity: 0.6;
+}
+		</style>
+	<?
+});
--- dsca-plugin/inc/change-twentyfifteen-theme-edits.php
+++ dsca-plugin/inc/change-twentyfifteen-theme-edits.php
@@ -10,3 +10,37 @@ add_action(
 		echo "<a href='/search'>Search</a> &nbsp; <span style='opacity:0.25'>|</span> &nbsp; ";
 	}
 );
+
+// microblog "aside" posts don't have post_titles, 2015 relies on them for next/prev links.
+// the post_tile filter cant pinpoint this one use area its needed. so refactor html output with new post_titles.
+// @see `the_post_navigation()` in `/themes/twentyfifteen/single.php`
+add_filter( 'next_post_link', 'fix_2015_nav_links', 10, 5);
+add_filter( 'previous_post_link', 'fix_2015_nav_links', 10, 5);
+function fix_2015_nav_links( $output, $format, $link, $post, $adjacent ) {
+	$title = has_post_format( 'aside', $post ) ? wp_trim_words( wp_strip_all_tags( $post->post_content ), 3 )."" : apply_filters('post_title',$post->post_title);
+
+	// veryify string as its not being escaped.
+	if ( ! in_array( $adjacent, ['next','previous'] ) )
+		die('something has gone terribly wrong');
+
+	return sprintf('
+		<div class="nav-'.$adjacent.'">
+			<a href="%s" rel="'.$adjacent.'">
+				<span class="meta-nav" aria-hidden="true">'.$adjacent.'</span>
+				<span class="screen-reader-text">'.$adjacent.' post:</span>
+				<span class="post-title">%s</span>
+			</a>
+		</div>',
+		get_permalink( $post ),
+		$title
+	);
+}
+
+// for whatever reason, this theme doesn't render favicon so well.. shimming this in:
+
+add_action('wp_head', function(){
+	echo '
+	<link rel="apple-touch-icon-precomposed" href="'.esc_url( dsca_get_gravatar_from_admin_email( 256 ) ).'">
+	<link rel="shortcut icon" href="'.esc_url( dsca_get_gravatar_from_admin_email( 256 ) ).'">
+	';
+}, 999);

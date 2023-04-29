eec3024c6da9f761d8a503de628c8d35197f2d29
David Sword <1996370+davidsword@users.noreply.github.com>
2023-04-28 20:30:18 -0700

    Update add-admin-reminder.php

--- wp-content/plugins/dsca-plugin/inc/add-admin-reminder.php
+++ wp-content/plugins/dsca-plugin/inc/add-admin-reminder.php
@@ -2,9 +2,11 @@
 
 
 add_action( 'admin_notices', function () {
+	if ( ! get_option('reminder') )
+		return;
     ?>
     <div class="notice notice-info">
-        <p><?php esc_html( get_option('reminder') ); ?></p>
+        <p><?php echo esc_html( get_option('reminder') ); ?></p>
     </div>
     <?php
 } );
